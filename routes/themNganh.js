8dc4d52430af96ef65fa472ec57642ea1c51571e
TranDinhDa <voanhdunglop63@gmail.com>
2021-07-19 18:29:01 +0700

    Ban Da Update Js for All form

--- routes/themNganh.js
+++ routes/themNganh.js
@@ -4,39 +4,51 @@ const router = express.Router()
 const Page_Nganh = require("../models/ThemNganh")
 
 router.get('/', (req, res, next) => {
-    if (req.cookies.userloginNumber==null || req.cookies.userloginNumber=="") {
+    if (req.cookies.userloginNumber == null || req.cookies.userloginNumber == "") {
         res.render("error")
-    } else if (req.cookies.userloginNumber==0) {
-        res.render("themNganh/themNganh")
+    } else if (req.cookies.userloginNumber == 0) {
+        res.render("themNganh/themNganh",{
+            tontai:false
+        })
     }
 })
 router.post("/signUp", (req, res, next) => {
-    const themNganh = new Page_Nganh(req.body)
-    themNganh.save()
-    res.redirect("/")
+    Page_Nganh.findOne({ nameNganh: req.body.nameNganh }, function (err, nganh) {
+        if (nganh) {
+            res.render("themNganh/themNganh",{
+                tontai:true,
+                ten:nganh.nameNganh
+            })
+        }
+        else {
+            const themNganh = new Page_Nganh(req.body)
+            themNganh.save()
+            res.redirect("/")
+        }
+    })
 })
 
-router.get('/:id/edit', (req, res,next) => {
-    if (req.cookies.userloginNumber==0) {
+router.get('/:id/edit', (req, res, next) => {
+    if (req.cookies.userloginNumber == 0) {
         Page_Nganh.findById(req.params.id)
-        .then(page => res.render("themNganh/editPage", {page}))
-        .catch(next)
+            .then(page => res.render("themNganh/editPage", { page }))
+            .catch(next)
     } else {
         res.render("error")
     }
 })
 router.post('/:id', (req, res, next) => {
     Page_Nganh.updateOne({ _id: req.params.id }, req.body)
-        .then(()=>res.redirect("/me"))
+        .then(() => res.redirect("/me"))
         .catch(next)
-    
+
 })
 
-router.get('/:id/delete', (req, res,next) => {
-    if (req.cookies.userloginNumber==0) {
+router.get('/:id/delete', (req, res, next) => {
+    if (req.cookies.userloginNumber == 0) {
         Page_Nganh.deleteOne({ _id: req.params.id })
-        .then(page => res.redirect("/me"))
-        .catch(next)
+            .then(page => res.redirect("/me"))
+            .catch(next)
     } else {
         res.render("error")
     }
